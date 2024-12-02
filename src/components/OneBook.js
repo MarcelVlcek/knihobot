76ef7a34ac1d66362b70919fee0bf788886955f8
Marcel Vlcek <vlcek.marc@gmail.com>
2024-12-02 11:53:27 +0100

    delete-button

--- src/components/OneBook.js
+++ src/components/OneBook.js
@@ -1,18 +1,33 @@
+import { useState } from 'react'
 import books from '../data/data'
 import './OneBook.css'
+import OrderButton from './OrderButton'
 
 const OneBook = () => {
+
+  const [bookList, setBookList] = useState(books)
+
+  const deleteOneBook = (id) => {
+    const filteredBooks = bookList.filter( (oneBook) => {
+      return oneBook.id !== id
+    } )
+
+    setBookList(filteredBooks)
+  }
+
+
   return (<div className='all-books'> 
     {
-      books.map((Book) => {
+      bookList.map((Book) => {
         const {id, image, author, title, numberOfProducts, price} = Book
       
         return <div className='one-book' key={id}>
             <img src={image} alt="book-photo" />
-            <h2>{author}</h2>
-            <h3>{title}</h3>
-            <p>{numberOfProducts}</p>
-            <p>{price}</p>
+            <a href='https://example.sk'>{author}</a>
+            <a href='https://example.sk'>{title}</a>
+            <a href='https://example.sk'>{numberOfProducts} ks</a>
+            <span>{price} kč</span>
+            <OrderButton deleteBook = { () => deleteOneBook(id) } ></OrderButton>
         </div>
       
       
---
+++ src/components/OrderButton.js
@@ -0,0 +1,9 @@
+
+
+const OrderButton = (props) => {
+  return (
+    <button onClick = {props.deleteBook} >Delete book</button>
+  )
+}
+
+export default OrderButton
